86c661f3c871c6d127efb236a3c47b51bd113f24
Piyush Shah <piyushshah159@gmail.com>
2025-04-01 14:06:57 +0530

    updated homepage->gender collection section

--- client/src/components/Products/GenderCollectionSection.jsx
+++ client/src/components/Products/GenderCollectionSection.jsx
@@ -10,7 +10,7 @@ function GenderCollectionSection() {
           <img src={womensCollectionImage} className="w-full h-[400px] md:h-[500px] lg:h-[600px] object-cover" />
           <div className="absolute bottom-8 left-8 bg-white/80 p-4">
             <h2 className="text-2xl font-bold text-gray-900 mb-2">Women's Collection</h2>
-            <Link to='/collections/all?gender=women' className="text-gray-900 underline">
+            <Link to='/collections/all?gender=Women' className="text-gray-900 underline">
               Shop Now
             </Link>
           </div>
@@ -19,7 +19,7 @@ function GenderCollectionSection() {
           <img src={mensCollectionImage} className="w-full h-[400px] md:h-[500px] lg:h-[600px] object-cover" />
           <div className="absolute bottom-8 left-8 bg-white/80 p-4">
             <h2 className="text-2xl font-bold text-gray-900 mb-2">Men's Collection</h2>
-            <Link to='/collections/all?gender=men' className="text-gray-900 underline">
+            <Link to='/collections/all?gender=Men' className="text-gray-900 underline">
               Shop Now
             </Link>
           </div>
